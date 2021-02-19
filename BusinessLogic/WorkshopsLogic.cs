260dd6ff349778b5692fa6be9bda62d59a64da3e
Rodrigo Heredia <rodrigoh1205@gmail.com>
2021-02-18 23:40:14 -0400

    Initial commit

---
+++ BusinessLogic/WorkshopsLogic.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace workshop_api
+{
+	public class WorkshopsLogic
+	{
+
+	}
+}
---
+++ Controllers/WorkshopsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace workshop_api.Controllers
+{
+	[ApiController]
+	[Route("workshops")]
+	public class WorkshopsController : ControllerBase
+	{
+		[HttpPost]
+		[Route("add-workshop")]
+		public int addWorkshop()
+		{
+			// Empty
+			return 200;
+		}
+	}
+}
