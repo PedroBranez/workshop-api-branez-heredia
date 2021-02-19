2ef6327dbeadaadd4dd288c31b5982596d217a17
Rodrigo Heredia <rodrigoh1205@gmail.com>
2021-02-19 10:02:11 -0400

    Added IWorkshopTableDB Interface

---
+++ Database/Database/IWorkshopTableDB.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Database.Database.Models;
+
+public interface IWorkshopTableDB
+{
+    public List<WorkShop> GetAll(); //Get, Read all in Database
+    public WorkShop Create(WorkShop workshop); //Post
+    public void Update(WorkShop workshop); //Put
+    public void Delete(WorkShop workshop); //Delete
+}
