2c88bf89332438df8465ebccf087e954cb0e9db3
Pedro Brañez <pedrobran8@gmail.com>
2021-02-19 00:07:41 -0400

    Add workshop model

---
+++ BusinessLogic/DTOModels/WorkShop.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Workshop
+{
+	public Workshop()
+	{
+        public string WorkshopID { get; set; }
+        public string WorkShopName { get; set; }
+        public string WorkshopStatus { get; set; }
+    }
+}
